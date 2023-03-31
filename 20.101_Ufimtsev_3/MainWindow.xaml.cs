2af6696c05f7282ba5d9682571dec39c984128e6
Кирилл Уфимцев <kirik21x@gmail.com>
2023-03-31 15:07:43 +0300

    Добавьте файлы проекта.

---
+++ 20.101_Ufimtsev_3/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using _20._101_Ufimtsev_3.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20._101_Ufimtsev_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            var db = Entities.GetContext().Discipline.Where(p => p.IdDiscipline == 2).ToList();
+
+            if (db == null)
+            {
+                MessageBox.Show("Ничего не найдено!");
+                return;
+            }
+
+            LoadData.ItemsSource = db;
+        }
+    }
+
+    
+}
